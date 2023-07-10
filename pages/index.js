939508b94e97518e1c007426d11e3667e474ab01
aaradhyakul <aaradhyakul@gmail.com>
2023-07-10 20:52:03 +0530

    redirect_error fixed

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,109 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const googleHandler = (e) => {
+    (e) => {
+      e.preventDefault();
+      signIn("google");
+    };
+  };
+  const gitHubHandler = (e) => {
+    (e) => {
+      e.preventDefault();
+      signIn("github");
+    };
+  };
+
+  if (!(status == "authenticated")) {
+    return (
+      <div
+        className={`${styles.home_container} w-screen min-h-screen flex md:flex-row flex-col`}
+      >
+        <div
+          className={`${styles.left} bg-[#000000] md:flex-left flex-top  color-[#FFFFFF] flex justify-center items-center`}
+        >
+          <h1 className={`text-white md:text-[72px] font-mont font-bold`}>
+            Board.{" "}
+          </h1>
+        </div>
+        <div
+          className={`${styles.right} md:flex-right flex-bottom bg-[#F5F5F5] flex justify-center items-center font-arial`}
+        >
+          <div
+            className={`w-[90%] md:max-w-[385px] flex flex-col gap-y-[10px]`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <h1 className={`text-4xl font-black tracking-normal font-mont`}>
+              Sign In
+            </h1>
+            <div className={`text-base`}>Sign in to your account</div>
+            <div className={`flex gap-x-[20px] mb-[8px]`}>
+              <Link
+                href="/api/auth/signin"
+                onClick={googleHandler}
+                className={`bg-white px-[15px] py-[5px] text-mid rounded-lg text-[#858585] flex-auto flex items-center justify-center gap-x-[12px] tracking-[0.2px]`}
+              >
+                <img width="14px" src="/static/google.png" />
+                Sign in with google
+              </Link>
+              <Link
+                href="/api/auth/signin"
+                onClick={gitHubHandler}
+                className={`bg-white px-[15px] py-[5px] text-mid rounded-lg text-[#858585] flex-auto flex items-center justify-center gap-x-[12px] tracking-[0.2px]`}
+              >
+                <img width="14px" src="/static/github.png" />
+                Sign in with Github
+              </Link>
+            </div>
+            <form
+              className={`${styles.email_form} md:text-base flex flex-col gap-[20px] px-[30px] py-[30px] rounded-2xl`}
+            >
+              <div
+                className={`${styles.email_form_control} flex flex-col gap-y-[5px]`}
+              >
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className={`md:text-base rounded-lg px-[10px] py-[5px] tracking-[0.3px]`}
+                />
+              </div>
+              <div
+                className={`${styles.email_form_control} flex flex-col gap-y-[5px]`}
+              >
+                <label>Password</label>
+                <input
+                  type="password"
+                  className={`md:text-base rounded-lg px-[15px] py-[5px] tracking-[0.3px]`}
+                />
+              </div>
+              <div className={`md:text-base text-formblue`}>
+                Forgot Password?
+              </div>
+              <button
+                type="submit"
+                className={`md:text-lg bg-black text-white rounded-lg py-[7px] font-bold`}
+              >
+                Sign In
+              </button>
+            </form>
+            <div className={`mx-auto`}>
+              <span className={`text-mid text-[#858585]`}>
+                {"Don't have an account?&nbsp;"}
+              </span>
+              <a className={`text-mid text-formblue `} href="#">
+                Register here
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
+  } else {
+    router.push("/dashboard");
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      flex: {
+        left: "1 1 588px",
+        right: "1 1 852px",
+        top: "0 1 100px",
+        bottom: "1 1 auto",
+      },
+      fontFamily: {
+        mont: ["Montserrat", "sans-serif"],
+        arial: ["Arial", "sans-serif"],
+      },
+      fontSize: {
+        mid: "14px",
+      },
+      colors: {
+        formblue: "#346BD4",
+      },
+      screens: {
+        mid: "1276px",
+        mh: "900px",
+        ce: "850px",
+        mb: "540px",
       },
     },
   },
   plugins: [],
-}
+};
